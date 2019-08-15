d5ca3cc44da4db240b2e3f624ae0e96a516c8bd7
Dobromir Marinov <dmarinov@msxi-euro.com>
2019-08-15 11:44:01 +0100

    Added init file to test dir and changed paths

--- test/test_algorithm.py
+++ test/test_algorithm.py
@@ -20,7 +20,7 @@ class TestAlgorithm(unittest.TestCase):
         hp_searcher = PredictiveHyperOpt(total_models_count=50, full_models_count=5, final_epoch=45, partial_epochs=[4, 1], batch_size=1024,
                                          final_models_count=20)
 
-        data = pd.read_csv('resources/simulation.txt', header=0)
+        data = pd.read_csv('test/resources/simulation.txt', header=0)
 
         # When
         average_accuracy = hp_searcher.simulate(data, total_runs=10)
@@ -32,18 +32,18 @@ class TestAlgorithm(unittest.TestCase):
         # Given
         expected = 31.0
         expected_architecture = CnnBaseArchitecture((32, 32, 3), 10)
-        expected_architecture = expected_architecture.build_from_file('resources/expected_architecture.txt')
+        expected_architecture = expected_architecture.build_from_file('test/resources/expected_architecture.txt')
         seed(13)
         set_random_seed(13)
 
-        x_train = np.load('resources/x_train.npy')
-        y_train = np.load('resources/y_train.npy')
-        x_test = np.load('resources/x_test.npy')
-        y_test = np.load('resources/y_test.npy')
+        x_train = np.load('test/resources/x_train.npy')
+        y_train = np.load('test/resources/y_train.npy')
+        x_test = np.load('test/resources/x_test.npy')
+        y_test = np.load('test/resources/y_test.npy')
 
         # Creating the architecture from file
         arch = CnnBaseArchitecture((32, 32, 3), 10)
-        arch = arch.build_from_file('resources/architecture.txt')
+        arch = arch.build_from_file('test/resources/architecture.txt')
 
         # Creating the generator
         generator = ModelGenerator(arch, 'keras', removal_rate=0.2, duplication_rate=0.2, amendment_rate=0.5, seed=13)
